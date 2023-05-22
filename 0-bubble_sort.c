4f3625df61f5eb1b67ff3b24d0b2b8d27ed018de
issaevis <evis.topollaj@gmail.com>
2023-05-22 04:02:34 -0700

    Added an implementation of the bubble sort algorithm in 0-bubble_sort.c . Also added other complimentary files that make it run

---
+++ 0-bubble_sort.c
@@ -0,0 +1,40 @@
+/**
+ * bubble_sort - Function that implements a bubble sort algorithm
+ *
+ * @array: the array we need to sort
+ * @size: the size of the array
+ *
+ * Return: Returns nothing
+ */
+
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, check = 0;
+	int temp;
+
+	while (check != 1)
+	{
+		for (i = 0; i <= size; i++)
+		{
+			if (i == size)
+				continue;
+
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+		}
+		for (i = 0; i <= size; i++)
+		{
+			if (i == size)
+				check = 1;
+			if (array[i] > array[i + 1])
+				break;
+		}
+	}
+}
